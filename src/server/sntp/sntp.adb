acaa16b541aec4caa00e4803fb5124d70fe75532
Johannes Kliemann <jk@jkliemann.de>
2018-02-08 19:42:27 +0100

    add basic ada sntp implementation

---
+++ src/server/sntp/sntp.adb
@@ -0,0 +1,35 @@
+package body sntp
+with
+    SPARK_Mode => On
+is
+
+    function c_connect(Host : System.Address; Length : Integer; Ai : libc.Addrinfo) return libc.Socket
+    with
+        SPARK_Mode => Off
+    is
+        S_Host : String (1 .. Length);
+        for S_Host'Address use Host;
+    begin
+        return connect(S_Host, Ai);
+    end c_connect;
+
+    function connect(Host : String; Ai : libc.Addrinfo) return libc.Socket
+    is
+        Addr_status : Integer := libc.getaddrinfo(Host, Ai);
+        Sock : libc.Socket := libc.getsocket(Ai);
+    begin
+        return Sock;
+    end connect;
+
+    function get_time(Sock : libc.Socket; Ai : libc.Addrinfo) return Timestamp
+    is
+        Msg : Message := ( Leap => AlarmCondition, Version => 2, Mode => Client, Poll => 4,
+            Precision => 0, Root_Delay => 0, Root_Dispersion => 0, Stratum => 0, others => 0);
+        Sent : Long_Integer := Send(Sock, Msg, Ai);
+    begin
+        Msg := Recv(Sock, Ai);
+        return Swap(Msg.Transmit_Timestamp_Sec) - Unix_Epoch;
+    end get_time;
+
+end sntp;
+        
