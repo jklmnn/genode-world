19447393e117c1fb5b4dcaf98579df098e5fdab0
Johannes Kliemann <jk@jkliemann.de>
2018-02-08 21:34:01 +0100

    add ada build files

--- src/server/sntp/target.mk
+++ src/server/sntp/target.mk
@@ -1,6 +1,7 @@
 TARGET = sntp
 LIBS = libc lwip libc_lwip_nic_dhcp libc_lwip
-SRC_CC = main.cc client.cc
+SRC_CC = main.cc client.cc libc_wrapper.cc
+SRC_ADA = libc.adb sntp.adb
 
 INC_DIR += $(PRG_DIR)
 
