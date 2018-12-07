14339a32b2b2c3da98ecde91724839823855a834
Johannes Kliemann <jk@jkliemann.de>
2018-12-07 13:25:26 +0100

    quaternion: add port and wip target.mk

---
+++ src/app/quaternion/target.mk
@@ -0,0 +1,20 @@
+QT5_PORT_DIR := $(call select_from_ports,qt5)
+QT5_CONTRIB_DIR := $(QT5_PORT_DIR)/src/lib/qt5/qt5
+
+QUATERION_DIR := $(call select_from_ports,quaternion)/qmatrixclient/quaternion
+QMATRIX_DIR := $(call select_from_ports,quaternion)/qmatrixclient/libqmatrixclient
+
+QMAKE_PROJECT_PATH = $(QUATERION_DIR)/client
+QMAKE_PROJECT_FILE = $(QUATERION_DIR)/quaternion.pro
+
+INC_DIR += $(QMATRIX_DIR)/lib
+
+vpath % $(QMAKE_PROJECT_PATH) $(QMATRIX_DIR)/lib
+
+include $(call select_from_repositories,src/app/qt5/tmpl/target_defaults.inc)
+include $(call select_from_repositories,src/app/qt5/tmpl/target_final.inc)
+
+LIBS += qt5_component qt5_printsupport stdcxx libm
+
+CC_CXX_OPT_STD = -std=gnu++14
+CC_CXX_WARN_STRICT =
